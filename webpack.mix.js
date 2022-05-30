5d2cf1cc1bb0f11cc6704533f68dca93236e405f
Samuel Meľuch <samuel.meluch@avast.com>
2022-05-30 18:46:59 +0200

    installed inertia, react

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,18 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .react()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .alias({
+        '@': 'resources/js',
+    }).browserSync({
+        proxy: "http://localhost:8000"
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
