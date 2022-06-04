c3125871bec36d6b5f82189c00209f7e1d76c2f0
Samuel Meľuch <samuel.meluch@avast.com>
2022-06-04 17:06:00 +0200

    augmented button

--- resources/js/Components/KeyboardControlsToneRadioSelect.js
+++ resources/js/Components/KeyboardControlsToneRadioSelect.js
@@ -7,15 +7,31 @@ const tones = ["c", "d", "e", "f", "g", "a", "b"];
 export default function KeyboardControlsToneRadioSelect(props) {
     const { scales, setScales } = useScaleContext();
     const [selected, setSelected] = useState(tones[0]);
+    const [halfTone, setHalfTone] = useState(false);
 
     useEffect(() => {
-        props.handleRadioToneChange(scales.currentScaleType, `${selected}4`);
-    }, [selected]);
+        let selectedTone = halfTone ? `${selected}#4` : `${selected}4`;
+        props.handleRadioToneChange(scales.currentScaleType, selectedTone);
+    }, [selected, halfTone]);
+
+    function handleAugmentedOrDiminishedButtonClick() {
+        halfTone ? setHalfTone(false) : setHalfTone(true);
+    }
 
     return (
         <div className="w-72 flex">
             <RadioGroup value={selected} onChange={setSelected}>
                 <div className="flex">
+                    <button className={`
+                            mx-1 relative flex cursor-pointer rounded-md w-12 h-8 shadow-md focus:outline-none text-m items-center justify-center
+                            ${halfTone ?
+                                'text-cyan-300 bg-cyan-900' : 'text-cyan-600 hover:bg-cyan-400 border-2 border-cyan-400'
+                            }
+                            `}
+                            onClick={handleAugmentedOrDiminishedButtonClick}
+                    >
+                        <span className="font-medium uppercase">#</span>
+                    </button>
                     {tones.map((tone) => (
                         <RadioGroup.Option
                             key={tone}
@@ -30,20 +46,19 @@ export default function KeyboardControlsToneRadioSelect(props) {
                         >
                             {({ active, checked }) => (
                                 <>
-                                    <div className="w-full h-full">
-                                        <div className="text-sm flex w-full h-full items-center justify-center">
-                                            <RadioGroup.Label
-                                                as="p"
-                                                className={`font-medium uppercase ${
-                                                    checked
-                                                        ? "text-amber-900"
-                                                        : "hover:text-amber-900"
-                                                }`}
-                                            >
-                                                {tone}
-                                            </RadioGroup.Label>
-                                        </div>
-                                    </div>
+                                      <div className="text-sm flex w-full h-full items-center justify-center">
+                                          <RadioGroup.Label
+                                              as="p"
+                                              className={`font-medium uppercase ${
+                                                  checked
+                                                      ? "text-amber-900"
+                                                      : "hover:text-amber-900"
+                                              }`}
+                                          >
+                                              {tone}
+                                          </RadioGroup.Label>
+                                      </div>
+
                                 </>
                             )}
                         </RadioGroup.Option>
