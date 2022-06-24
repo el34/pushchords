1fb29e7d5118fc3c61b03ea307ac9c66548dac8e
Samuel Meľuch <samuel.meluch@avast.com>
2022-06-24 16:10:33 +0200

    button rounded

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -11,7 +11,7 @@ export default function Welcome(props) {
                 <div className="flex flex-col w-full xl:w-2/5 justify-start lg:items-start overflow-y-hidden">
                     <h1 className="my-4 text-3xl md:text-6xl text-indigo-800 font-oswald font-black uppercase leading-tight text-center md:text-left slide-in-bottom-h1">Scales & chords cheatsheet</h1>
                     <p className="leading-normal text-base md:text-xl mb-8 font-light text-center md:text-left slide-in-bottom-subtitle">Apple pie toffee lollipop chocolate lemon drops marshmallow cheesecake tootsie roll. Chupa chups cake lollipop danish apple pie muffin.</p>
-                    <Link className="text-white text-center uppercase font-bold text-sm tracking-widest hover:bg-green-500 bg-green-400 rounded-sm px-8 py-2 no-underline hover:no-underline" href="/scales">
+                    <Link className="text-white text-center uppercase font-bold text-sm tracking-widest hover:bg-green-500 bg-green-400 rounded-md inline-block px-8 py-2 no-underline hover:no-underline" href="/scales">
                         Try for free
                     </Link>
                 </div>
